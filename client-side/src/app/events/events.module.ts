753a26a913ec7be0f8ebf45250d821e99db27286
roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>
2022-07-24 11:47:44 +0300

    Added events list
    * DI-20874-implement events list
    
    * hide search bar
    
    * fix generic list height
    
    * fix delete event
    
    * version bump
    
    * PR comments

--- client-side/src/app/events/events.module.ts
+++ client-side/src/app/events/events.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 import { PepAddonService } from '@pepperi-addons/ngx-lib';
-import { PepPageLayoutModule} from '@pepperi-addons/ngx-lib/page-layout'
-import { PepTopBarModule} from '@pepperi-addons/ngx-lib/top-bar'
-import { PepSizeDetectorModule} from '@pepperi-addons/ngx-lib/size-detector'
+import { PepPageLayoutModule} from '@pepperi-addons/ngx-lib/page-layout';
+import { PepTopBarModule} from '@pepperi-addons/ngx-lib/top-bar';
+import { PepSizeDetectorModule} from '@pepperi-addons/ngx-lib/size-detector';
+import { PepGenericListModule } from '@pepperi-addons/ngx-composite-lib/generic-list';
 
 import { EventsComponent } from './index';
+import { EventsService } from '../services/events-service'
 
 import { config } from '../addon.config';
+import { PepButtonModule } from '@pepperi-addons/ngx-lib/button';
 
 export const routes: Routes = [
     {
@@ -26,6 +29,8 @@ export const routes: Routes = [
         PepPageLayoutModule,
         PepTopBarModule,
         PepSizeDetectorModule,
+        PepGenericListModule,
+        PepButtonModule,
         TranslateModule.forChild({
             loader: {
                 provide: TranslateLoader,
@@ -39,6 +44,7 @@ export const routes: Routes = [
     exports: [EventsComponent],
     providers: [
         TranslateStore,
+        EventsService,
         // Add here all used services.
     ]
 })
