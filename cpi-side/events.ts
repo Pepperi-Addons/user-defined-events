b55c6c8d1bdc61ad07d76da8dc6bac592cebf757
roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>
2022-12-21 15:14:58 +0200

    Di 22091 accumulate value (#20)
    * DI-22091 aAccumulate return value from all logic blocks
    
    * change return value

--- cpi-side/events.ts
+++ cpi-side/events.ts
@@ -11,6 +11,7 @@ export async function load(configuration: any) {
     console.log(`after getting events, received: ${JSON.stringify(events)}`);
     events.forEach(event => {
         const filter = event.EventField ? { FieldID: event.EventField, ...event.EventFilter } : event.EventFilter;
+        let result: any = {};
         pepperi.events.intercept(event.EventKey as any, filter, async (data, next, main) => {
             const groups: Map<number,LogicBlock[]> = groupBy(event.LogicBlocks, x => !!Number(x.ParallelExecutionGroup) ? Number(x.ParallelExecutionGroup): Number.MAX_VALUE);
             const sorted = new Map([...groups.entries()].sort((a,b) => {
@@ -19,30 +20,39 @@ export async function load(configuration: any) {
             console.log(`inside ${event.EventKey} event. \n after group by. groups: ${JSON.stringify(sorted.entries())}`);
             for await (const groupKey of sorted.keys()) {
                 const blocksToRun = sorted.get(groupKey)?.filter(val => !val.Disabled);
-                await handleGroupBlocks(blocksToRun, data, groupKey != Number.MAX_VALUE)
+                const value = await handleGroupBlocks(blocksToRun, data, groupKey != Number.MAX_VALUE);
+                result = {...result, ...value};
             }
 
             await next(main);
+
+            return result;
         })
     });
 }
 
-async function handleGroupBlocks(blocks?: LogicBlock[], eventData?: any, parallelExecution: boolean = false) {
+async function handleGroupBlocks(blocks?: LogicBlock[], eventData?: any, parallelExecution: boolean = false): Promise<any> {
+    let result: any = {};
     if(blocks && blocks.length > 0) {
         if(parallelExecution) {
-            await Promise.all(blocks.map(block => {
+            result = (await Promise.all(blocks.map(block => {
                 return runSingleBlock(block, eventData);
-            }))
+            }))).reduce((prev, current) => {
+                prev = {...prev, ...current};
+                return prev;
+            }, {})
         }
         else {
             for (const block of blocks) {
-                await runSingleBlock(block, eventData);
+                const value = await runSingleBlock(block, eventData);
+                result = {...result, ...value}
             }
         }
     }
+    return result;
 }
 
-async function runSingleBlock(block: LogicBlock, data) {
+async function runSingleBlock(block: LogicBlock, data): Promise<any> {
     return pepperi.addons.api.uuid(block.Relation.AddonUUID).post({
         url: block.Relation.BlockExecutionRelativeURL,
         body: block.Configuration,
